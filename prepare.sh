9e6e2b287d084403fc9d9bb2eea3cd593db24901
Ronald <ronald.ham@gmail.com>
2019-06-04 15:59:43 +0200

    initial

---
+++ prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# create nfs mount
+mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE-$STACK_VERSION/data
+
+# remove any old secrest and configs
+docker secret rm $(docker secret ls -f name=keycloack -q)
+
+
+# create secrets for database
+# alternative date |md5sum|awk '{print $1}' | docker secret create my_secret -
+date |md5sum|awk '{print $1}' | docker secret create keycloak_db_dba_password -
+date |md5sum|awk '{print $1}' | docker secret create keycloak_password -
+
